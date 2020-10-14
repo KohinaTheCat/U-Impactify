90b92e668aea0bd2c2cc1011fcbadd7acced8399
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2020-10-13 22:19:47 -0400

    [BOUN-23] created an empty starter angular project

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,7 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo(): Promise<unknown> {
+    return browser.get(browser.baseUrl) as Promise<unknown>;
+  }
+}
