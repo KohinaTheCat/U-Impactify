cadff60b16ced85e23252ffe4317c1010a52b447
Samyakk123 <samyak.mehta@mail.utoronto.ca>
2020-10-15 16:21:09 -0400

    [BOUN-27] added the necessary files.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupQuestionaireComponent } from './pages/signup-questionaire/signup-questionaire.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupQuestionaireComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/pages/signup-questionaire/signup-questionaire.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-questionaire',
+  templateUrl: './signup-questionaire.component.html',
+  styleUrls: ['./signup-questionaire.component.css']
+})
+export class SignupQuestionaireComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
