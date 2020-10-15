06975bc90725630ea1a88d7f8544d61304f4d143
KohinaTheCat <clarachick11@hotmail.com>
2020-10-15 14:38:59 -0400

    BOUN-24 Added responsive web design for the Signup menu

---
+++ frontend/src/app/pages/login-signup/FormDisplay.ts
@@ -0,0 +1,5 @@
+export interface FormDisplay{
+    greeting: string,
+    linkPrompt: string,
+    link: string
+}
--- frontend/src/app/pages/login-signup/login-signup.component.ts
+++ frontend/src/app/pages/login-signup/login-signup.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormDisplay } from './FormDisplay';
 
 @Component({
   selector: 'app-login-signup',
   templateUrl: './login-signup.component.html',
-  styleUrls: ['./login-signup.component.css']
+  styleUrls: ['./login-signup.component.css'],
 })
 export class LoginSignupComponent implements OnInit {
+  form: FormDisplay = {
+    greeting: 'Create an Account',
+    linkPrompt: 'Already have an account?',
+    link: 'Login',
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
