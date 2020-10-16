2d2eafa50d7b018d0d9cbe5fbf9444f9c6883e9c
booooper <pandousa@hotmail.com>
2020-10-16 14:14:41 -0400

    BOUN-31 Added base files for course creation service

---
+++ frontend/src/app/services/create-course.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CreateCourseService {
+
+  constructor(private http: HttpClient) { }
+}
