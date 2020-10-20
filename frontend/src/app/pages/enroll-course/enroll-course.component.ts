41c01ac503688e3d2cfd259931f87edc7f2eccc0
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2020-10-20 00:02:27 -0400

    [BOUN-38] added courses show in add new course student

--- frontend/src/app/pages/enroll-course/enroll-course.component.ts
+++ frontend/src/app/pages/enroll-course/enroll-course.component.ts
@@ -1,37 +1,36 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { CourseService } from 'src/app/services/course.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-enroll-course',
   templateUrl: './enroll-course.component.html',
-  styleUrls: ['./enroll-course.component.css']
+  styleUrls: ['./enroll-course.component.css'],
 })
 export class EnrollCourseComponent implements OnInit {
-
   basic: boolean = true;
-  error : string = '';
-  constructor(private router: Router, private courseService: CourseService) { }
+  error: string = '';
+  courses: any[] = [];
+
+  selectedId: string = "";
+
+  constructor(private router: Router, private courseService: CourseService, private userService: UserService) {}
 
   ngOnInit(): void {
+    this.courseService.getAllCourses().subscribe((res) => {
+      res.forEach(course => {
+        const {_id, title, description, level} = course;
+        this.courses.push({ _id, title, description, level });
+      });
+    });
   }
 
-  cancel(){
+  cancel() {
     this.router.navigate(['dashboard']);
   }
 
-  courseHandler(){
-    console.log("I made it !");
-    this.courseService.getAllCourses().subscribe(
-      (res) => {
-        for(let i=0; i<res.length; i++){
-          console.log(res[i].title);
-        }
-      }
-    )
-
+  courseHandler() {
+     // this.courseService.enrollUser(this.userService.getCurrentUser()._id, selectedId);
   }
-
-
-
 }
