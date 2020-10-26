45431d1e4c254502a7f0d0bd39b3f26835acb48d
AryPat <69769265+AryPat@users.noreply.github.com>
2020-10-25 22:17:56 -0400

    [BOUN-51] Added To usermodel in backend and frontend

--- frontend/src/app/models/user.model.ts
+++ frontend/src/app/models/user.model.ts
@@ -1,10 +1,11 @@
-export interface User{
-  "_id": string,
-  "username": string,
-  "email": string,
-  "password": string,
-  "type": string,
-  "classesEnrolled": object[];
-  "classesTeaching": object[];
-  "questionaire": string[][];
+export interface User {
+  _id: string;
+  username: string;
+  email: string;
+  password: string;
+  type: string;
+  classesEnrolled: object[];
+  classesTeaching: object[];
+  questionaire: string[][];
+  socialInitiative: string;
 }
