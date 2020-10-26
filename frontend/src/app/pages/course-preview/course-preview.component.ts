db343f0d22c7c28d0ed472ff3547ac98414d3960
booooper <pandousa@hotmail.com>
2020-10-26 14:41:03 -0400

    BOUN-43 IL has button to enroll in course preview, need to design

--- frontend/src/app/pages/course-preview/course-preview.component.ts
+++ frontend/src/app/pages/course-preview/course-preview.component.ts
@@ -1,3 +1,5 @@
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user.model';
 import { Observable } from 'rxjs';
 import { CourseService } from 'src/app/services/course.service';
 import { Router } from '@angular/router';
@@ -8,43 +10,61 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-course-preview',
   templateUrl: './course-preview.component.html',
-  styleUrls: ['./course-preview.component.css']
+  styleUrls: ['./course-preview.component.css'],
 })
+
 export class CoursePreviewComponent implements OnInit {
 
-
-  // course = {
-  //   title: 'coursename',
-  //   description: 'coursedescription',
-  //   teachers: ['instructor1', 'instructor2'],
-  //   tags: 'coursetags',
-  //   level: 'courselevel',
-  // };  
-
   course = {
+    _id: '',
     title: '',
     description: '',
     teachers: [],
     tags: '',
     level: '',
-  }; 
+  };
 
-  // course: Course;
+  user: User;
 
-  constructor(private activatedRouter: ActivatedRoute, private courseService: CourseService,) {}
+  constructor(
+    private userService: UserService,
+    private activatedRouter: ActivatedRoute,
+    private courseService: CourseService,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
+    this.user = this.userService.getCurrentUser();
     const title = this.activatedRouter.snapshot.params['id'];
-    this.courseService.getCourse(title).subscribe(
-      (incomingCourse: Course) => {
-        this.course.title = incomingCourse[0].title;
-        this.course.description = incomingCourse[0].description;
-        this.course.teachers = incomingCourse[0].teachers;
-        this.course.tags = incomingCourse[0].tags;
-        this.course.level = incomingCourse[0].level;
-      }
-    );
-    console.log(this.course)
+    this.courseService.getCourse(title).subscribe((incomingCourse: Course) => {
+      this.course.title = incomingCourse[0].title;
+      this.course.description = incomingCourse[0].description;
+      this.course.teachers = incomingCourse[0].teachers;
+      this.course.tags = incomingCourse[0].tags;
+      this.course.level = incomingCourse[0].level;
+      this.course._id = incomingCourse[0]._id;
+    });
+    console.log(this.course);
   }
 
+  enrollHandler() {
+    this.courseService
+      .enrollInCourse(this.userService.getCurrentUser()._id, this.course._id)
+      .subscribe(
+        (res) => {
+          console.log(res);
+          this.userService
+            .enrollInCourse(this.userService.getCurrentUser()._id, {
+              _id: this.course._id,
+              name: this.course.title,
+            })
+            .subscribe(
+              (res) => console.log(res),
+              (err) => console.log(err)
+            );
+        },
+        (err) => console.log(err)
+      );
+    this.router.navigate(['dashboard']);
+  }
 }
