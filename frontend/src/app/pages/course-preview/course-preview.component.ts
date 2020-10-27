86d1b3020e0f4a0002e3a375fed29ebb3b35514d
booooper <pandousa@hotmail.com>
2020-10-26 20:51:42 -0400

    BOUN-43 if learner already enrolled in course, button is not shown for them

--- frontend/src/app/pages/course-preview/course-preview.component.ts
+++ frontend/src/app/pages/course-preview/course-preview.component.ts
@@ -19,12 +19,14 @@ export class CoursePreviewComponent implements OnInit {
     _id: '',
     title: '',
     description: '',
+    students: [],
     teachers: [],
     tags: '',
     level: '',
   };
 
   valid : boolean;
+  alreadyEnrolled : boolean = false;
   error : string;
   user: User;
 
@@ -43,10 +45,17 @@ export class CoursePreviewComponent implements OnInit {
       this.valid = true;
       this.course.title = incomingCourse[0].title;
       this.course.description = incomingCourse[0].description;
+      this.course.students = incomingCourse[0].students;
       this.course.teachers = incomingCourse[0].teachers;
       this.course.tags = incomingCourse[0].tags;
       this.course.level = incomingCourse[0].level;
       this.course._id = incomingCourse[0]._id;
+
+      for(let i=0;i<this.course.students.length ;i++){ 
+        if(this.course.students[i] == this.user._id){
+          this.alreadyEnrolled = true;
+        }
+     }
     },
     (err) => {
       this.valid = false;
@@ -59,6 +68,7 @@ export class CoursePreviewComponent implements OnInit {
   }
 
   enrollHandler() {
+    // call another services to check if user already enrolled in course
     this.courseService
       .enrollInCourse(this.userService.getCurrentUser()._id, this.course._id)
       .subscribe(
