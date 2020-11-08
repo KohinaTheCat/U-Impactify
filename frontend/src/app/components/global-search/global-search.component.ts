214e1c9efdaf64fbd4f9d951fcff4f17e046dbb8
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2020-11-08 14:08:19 -0500

    [BOUN-91] fixed alignment

--- frontend/src/app/components/global-search/global-search.component.ts
+++ frontend/src/app/components/global-search/global-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 import { filter } from 'rxjs/operators';
@@ -8,17 +8,21 @@ import { filter } from 'rxjs/operators';
   templateUrl: './global-search.component.html',
   styleUrls: ['./global-search.component.css'],
 })
-export class GlobalSearchComponent implements OnInit {
+export class GlobalSearchComponent implements OnInit, AfterViewInit {
   constructor(private userService: UserService, private router: Router) {
     router.events
       .pipe(filter((event) => event instanceof NavigationEnd))
       .subscribe((event: NavigationEnd) => {
-       this.title =
-         event.url === '/'
-           ? 'Dashboard'
-           : event.url.substring(1).split('/')[0];
+        this.title =
+          event.url === '/'
+            ? 'Dashboard'
+            : event.url.substring(1).split('/')[0];
       });
   }
+
+  @ViewChild('searchBar') searchBar;
+  @ViewChild('barContainer') barContainer;
+  @ViewChild('dropdown') searchDropdown;
 
   title: String = '';
   searchQuery: String = '';
@@ -31,18 +35,37 @@ export class GlobalSearchComponent implements OnInit {
         : location.pathname.substring(1).split('/')[0];
   }
 
+  ngAfterViewInit(): void {
+    // @ts-ignore
+    const observer = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {     
+          this.searchDropdown.nativeElement.style.left = this.getOffset(this.searchBar.nativeElement) + "px";
+          this.searchDropdown.nativeElement.style.width = this.searchBar.nativeElement.clientWidth + "px";
+          
+      });
+    });
+
+    observer.observe(this.barContainer.nativeElement);
+  }
 
   logOut(): void {
     this.userService.setUser(null);
     this.router.navigate(['signup']);
   }
 
-  goToProfile(): void{
+  goToProfile(): void {
     this.router.navigate([`user/${this.userService.getCurrentUser()._id}`]);
   }
 
   onPressSearch(type: string): void {
-    this.router.navigate([`search/${type}/${encodeURI(this.searchQuery.trim() as string)}`]);
+    this.router.navigate([
+      `search/${type}/${encodeURI(this.searchQuery.trim() as string)}`,
+    ]);
     this.searchQuery = '';
+  }
+
+  getOffset(el: any) {
+    const rect = el.getBoundingClientRect();
+    return rect.left + window.scrollX;
   }
 }
