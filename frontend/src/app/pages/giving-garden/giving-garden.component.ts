3fe95f8794ba7e130bb284c70bd6ea9e4cdbcd0a
KohinaTheCat <clarachick11@hotmail.com>
2020-11-09 17:50:29 -0500

    BOUN-95 Done backend and backend

--- frontend/src/app/pages/giving-garden/giving-garden.component.ts
+++ frontend/src/app/pages/giving-garden/giving-garden.component.ts
@@ -1,20 +1,30 @@
+import { UserService } from 'src/app/services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-giving-garden',
   templateUrl: './giving-garden.component.html',
-  styleUrls: ['./giving-garden.component.css']
+  styleUrls: ['./giving-garden.component.css'],
 })
 export class GivingGardenComponent implements OnInit {
   opened: Boolean = false;
+  si: [any] = [''];
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit(): void {
+    this.userService.getAllSI().subscribe((res) => {
+      this.si = res;
+      console.log(this.si);
+    });
   }
 
-  donate(){
+  donate() {
     this.opened = !this.opened;
   }
 
+  learnMore(id): void {
+    this.router.navigate([`user/${id}`]);
+  }
 }
