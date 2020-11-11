4febd2e1adf5612b29d15f1a640e4fc17a197543
Samyakk123 <samyak.mehta@mail.utoronto.ca>
2020-11-11 02:39:02 -0500

    BOUN-86 Added as a model to create a working demonstration. Have not commited the assessment model yet.

--- frontend/src/app/pages/assessments/assessments.component.ts
+++ frontend/src/app/pages/assessments/assessments.component.ts
@@ -4,11 +4,13 @@ import { Course } from 'src/app/models/course.model';
 import { CourseService } from 'src/app/services/course.service';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/models/user.model';
+import { Assessments } from 'src/app/models/assessments.model';
 import {
   FileSystemDirectoryEntry,
   FileSystemFileEntry,
   NgxFileDropEntry,
 } from 'ngx-file-drop';
+
 import { ROOT_DROPDOWN_PROVIDER } from '@clr/angular/popover/dropdown/providers/dropdown.service';
 import { FiltersProvider } from '@clr/angular/data/datagrid/providers/filters';
 
@@ -22,15 +24,19 @@ export class AssessmentsComponent implements OnInit {
   user: User;
   openModal: boolean;
   array: string[] = ['1', '2', '3', '4', '5', '6'];
-  name: String;
+  name: String = '';
   files: NgxFileDropEntry[] = [];
   img: NgxFileDropEntry[] = [];
   imageError: string = '';
-  visibility: String;
+  visibility: boolean = false;
   error: string = '';
   basic: boolean = true;
 
-  //assessments: Array<assessments> = [];
+  courseId: String = '';
+  studentSubm: String[][] = [[]];
+  assessArr: Assessments[] = [];
+
+  file: String[] = [];
 
   constructor(
     private courseService: CourseService,
@@ -43,7 +49,7 @@ export class AssessmentsComponent implements OnInit {
     this.user = this.userService.getCurrentUser();
     this.openModal = false;
     this.name = '';
-    this.visibility = '';
+    this.visibility = false;
 
     const id = this.activatedRouter.snapshot.params['id'];
     this.courseService.getCourse(id).subscribe((incomingCourse: Course) => {
@@ -52,7 +58,15 @@ export class AssessmentsComponent implements OnInit {
   }
 
   registerHandler() {
-    console.log('Here: ' + this.name);
+    this.assessArr.push({
+      courseId: this.courseId,
+      name: this.name,
+      visibility: this.visibility,
+      files: this.file,
+      studentSubmissions: this.studentSubm,
+    });
+
+    console.log('Here: ' + this.assessArr[0].name);
     // const { name, visibility, files } = this;
     // const assessments = {
     //   name,
