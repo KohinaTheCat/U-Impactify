8782db0d418da79839eeccfc9002d8dd956d4631
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2020-11-13 18:43:53 -0500

    [BOUN-102] solutions student page done

--- frontend/src/app/pages/home-pages/solutions/solutions.component.ts
+++ frontend/src/app/pages/home-pages/solutions/solutions.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-solutions',
   templateUrl: './solutions.component.html',
-  styleUrls: ['./solutions.component.css']
+  styleUrls: ['./solutions.component.css'],
 })
 export class SolutionsComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  isStudent: boolean = true;
+
+  onPress(): void {
+    this.router.navigateByUrl('/signup');
   }
-
-  isStudent: boolean = false;
-
 }
