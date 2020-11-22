36875f88a40eb85a1b4a8ac9e65a157073151a71
AryPat <69769265+AryPat@users.noreply.github.com>
2020-11-21 23:26:45 -0500

    BOUN-111 Layouted Assessment page with proper routing :D

--- frontend/src/app/pages/studentSubmissionFolder/student-submission/student-submission.component.ts
+++ frontend/src/app/pages/studentSubmissionFolder/student-submission/student-submission.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseService } from 'src/app/services/course.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-student-submission',
   templateUrl: './student-submission.component.html',
-  styleUrls: ['./student-submission.component.css']
+  styleUrls: ['./student-submission.component.css'],
 })
 export class StudentSubmissionComponent implements OnInit {
+  courseId: string;
+  assessment: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private courseService: CourseService,
+    private router: Router,
+    private userService: UserService,
+    private activatedRouter: ActivatedRoute
+  ) {
+    const id = this.activatedRouter.snapshot.params['id'];
+    const assessmentId = this.activatedRouter.snapshot.params['assessment'];
+    this.assessment = assessmentId;
+    this.courseId = id;
   }
 
+  ngOnInit(): void {}
+
+  back() {
+    console.log('why');
+    this.router.navigate([`../../../course/${this.courseId}/assessments/`]);
+  }
 }
