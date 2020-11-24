dcea95251fc69bb1cdea743810f2e040591ac342
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2020-11-23 19:52:57 -0500

    [BOUN-128] enter button in front page header

--- frontend/src/app/components/front-page-header/front-page-header.component.ts
+++ frontend/src/app/components/front-page-header/front-page-header.component.ts
@@ -1,24 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-front-page-header',
   templateUrl: './front-page-header.component.html',
-  styleUrls: ['./front-page-header.component.css']
+  styleUrls: ['./front-page-header.component.css'],
 })
 export class FrontPageHeaderComponent implements OnInit {
+  constructor(private router: Router, private userService: UserService) {}
 
-  constructor(private router: Router) { }
+  user: User;
 
   ngOnInit(): void {
+    this.user = this.userService.getCurrentUser();
   }
 
   shouldShow(): Boolean {
-    return this.router.url !== "/signup";
+    return this.router.url !== '/signup';
   }
 
   navigate(): void {
     this.router.navigate(['signup']);
   }
-
 }
